5cf49b7c65468d655366b415b0be959ab46798f0
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2024-02-19 18:35:48 +0530

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Posts from './components/Posts';
+import About from './components/About';
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+
 
 function App() {
 
   return (
     <>
-      <div className=" bg-gray-50 flex flex-col flex-wrap p-4 m-4 rounded-md">
-        <a href="https://github.com/akshaysrivastava113" target="_blank" className=" p-2 m-2 w-72 text-center rounded-lg bg-gray-700 hover:bg-gray-800 hover:border-slate-50 text-white">Github</a>
-        <a href="https://medium.com/@akshaysrivastava0311" target="_blank" className=" p-2 m-2 w-72 text-center rounded-lg">Medium</a>
-        <a href="https://www.linkedin.com/in/akshaysrivastava113/" target="_blank" className=" p-2 m-2 w-72 text-center rounded-lg">LinkedIn</a>
-      </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Homepage/>} />
+        <Route path="/posts" element={<Posts/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Route, useNavigate } from "react-router-dom";
+
+export default function Header() {
+    const navigate = useNavigate();
+    return(
+        <div className="w-full h-16 flex justify-between items-center border-b-2 border-gray-300">
+        <div>
+        <a className="nav-link-style hover:text-blue-400 hover:cursor-pointer" onClick={() => {navigate("/")}}>Home</a>
+        </div>
+        <div>
+            <a className="nav-link-style hover:text-blue-400 hover:cursor-pointer" onClick={() => {navigate("/posts")}}>Posts</a>
+            <a className="nav-link-style hover:text-blue-400 hover:cursor-pointer" onClick={() => {navigate("/about")}}>About</a>
+        </div>
+        </div>
+    )
+}
